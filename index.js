c9dbec13e1d9c30bae2e9bd96398534c7b136c2b
kingsley37 <119013227+kingsley37@users.noreply.github.com>
2022-12-06 20:38:52 -0500

    another commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 console.log("It works!")
 
+var myName = "kingsley";
+let myLast = "Adu";
 
+const myMiddle = "Akwasi";
+
+
+
+
+console.log(myName)
+
+myName ="kingsley";
+
+
+var age = 37
+
+var isCool = true
+
+console.log(isCool)
